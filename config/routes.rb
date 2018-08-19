e71da1ac24f59e11f8350fcddde4a56b9138f454
adomant <m.facepalm@gmail.com>
2018-08-19 13:28:44 +0300

    created controllers for article, also created views and header

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/index', to: 'articles#index'
+  resources :articles
+  root 'articles#index'
 end
