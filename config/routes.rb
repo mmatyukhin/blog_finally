375bf225953a31c06d4a2823398dee8366b0d77b
adomant <m.facepalm@gmail.com>
2018-08-20 19:31:28 +0300

    created admin dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :admin do
+      resources :articles
+      resources :users
+
+      root to: "articles#index"
+    end
   get '/index', to: 'articles#index'
   resources :articles
   root 'articles#index'
