c00fd917c061099e416a1a7ccfe0e765b8c60114
adomant <m.facepalm@gmail.com>
2018-08-21 01:43:22 +0300

    added default image for administrate

--- app/fields/image_field.rb
+++ app/fields/image_field.rb
@@ -1,8 +1,15 @@
 require "administrate/field/base"
 
 class ImageField < Administrate::Field::Base
+    def default_url
+      'default.png'
+  end
   def url
+    if data.url.present?
     data.url
+  else
+    default_url
+  end
   end
   def to_s
     data
