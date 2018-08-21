3d664b47f98d11f8afb6deda4d72bf0562c20d25
adomant <m.facepalm@gmail.com>
2018-08-22 01:05:27 +0300

    added user and email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   has_many :articles
+  validates :username,  presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                   format: { with: VALID_EMAIL_REGEX }
 end
