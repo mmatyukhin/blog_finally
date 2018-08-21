64a24231f02ab405014721f8ddeabb4edc01a305
adomant <m.facepalm@gmail.com>
2018-08-21 23:09:02 +0300

    added constant for pagination

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
-  def index
-   @articles = Article.paginate(page: params[:page], :per_page => 3)
-  end
+  RESULTS_PER_PAGE = 3
+    def index
+      @articles = Article.paginate(page: params[:page], per_page:(RESULTS_PER_PAGE))
+    end
 end
