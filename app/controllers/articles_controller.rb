5a674fb72bff5fccce9b489408117a0f8eb3351b
adomant <m.facepalm@gmail.com>
2018-08-22 00:37:39 +0300

    skobochki

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,9 +3,9 @@ class ArticlesController < ApplicationController
   RESULTS_PER_PAGE = 3
 
   def index
-      @articles = Article.paginate(page: params[:page], per_page:(RESULTS_PER_PAGE))
+      @articles = Article.paginate(page: params[:page], per_page: RESULTS_PER_PAGE)
   end
-  
+
   def show
       @article = Article.find(params[:id])
   end
