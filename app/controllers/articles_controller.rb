88d423eafa37f084be7ec2de34fa71ceb50b6d39
adomant <m.facepalm@gmail.com>
2018-08-21 23:55:46 +0300

    fixed index and controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,10 @@
 class ArticlesController < ApplicationController
   RESULTS_PER_PAGE = 3
-    def index
+
+  def index
       @articles = Article.paginate(page: params[:page], per_page:(RESULTS_PER_PAGE))
-    end
+  end
+  def show
+      @article = Article.find(params[:id])
+  end
 end
