a3b3a6f48b73b22d867ed298a97308eb67f57b65
adomant <m.facepalm@gmail.com>
2018-08-23 21:45:12 +0300

    added image fallback

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -20,7 +20,9 @@ class ImageUploader < CarrierWave::Uploader::Base
   #
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
-
+  def default_url(*args)
+    [version_name, "default.png"].compact.join('_')
+  end
   # Process files as they are uploaded:
   # process scale: [200, 300]
   #
