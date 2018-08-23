8965d4cdc1cbc38eea8a6a9c57f051eafd8f37a9
adomant <m.facepalm@gmail.com>
2018-08-23 22:50:42 +0300

    indentation added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,12 +3,7 @@ class ArticlesController < ApplicationController
 
   def index
       @articles = Article.all
-  if params[:tag]
-    @articles = Article.tagged_with(params[:tag])
-    else
-    @articles = Article.all
-    end
-    @articles = @articles.order(created_at: :desc).page params[:page]
+      @articles = @articles.order(created_at: :desc).page params[:page]
   end
 
   def show
