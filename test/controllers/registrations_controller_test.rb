3bca859304938c6089fd12835c0076890b038cfb
adomant <m.facepalm@gmail.com>
2018-09-30 20:39:47 +0300

    deleted useless files

--- test/controllers/registrations_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class RegistrationsControllerTest < ActionDispatch::IntegrationTest
-  test "should get devise" do
-    get registrations_devise_url
-    assert_response :success
-  end
-
-end
