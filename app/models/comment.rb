2f3f3104382c7c260dcb478a8a6acc124380288e
adomant <m.facepalm@gmail.com>
2018-10-17 00:13:19 +0300

    fixes from review

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :article
   belongs_to :user
-  scope :approved, -> {where(moderation: true)}
+  scope :approved, -> { where(moderation: true) }
   validates :body,  presence: true, length: { minimum: 50, maximum: 250 }
 end
