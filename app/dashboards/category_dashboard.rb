7d7d190caacd5afd7022d0896ec6b6db28af31d8
adomant <m.facepalm@gmail.com>
2018-10-16 13:17:25 +0300

    added categories dashboard,also updated articles dashboard

--- app/dashboards/category_dashboard.rb
+++ app/dashboards/category_dashboard.rb
@@ -1,6 +1,6 @@
 require "administrate/base_dashboard"
 
-class ArticleDashboard < Administrate::BaseDashboard
+class CategoryDashboard < Administrate::BaseDashboard
   # ATTRIBUTE_TYPES
   # a hash that describes the type of each of the model's fields.
   #
@@ -8,14 +8,9 @@ class ArticleDashboard < Administrate::BaseDashboard
   # which determines how the attribute is displayed
   # on pages throughout the dashboard.
   ATTRIBUTE_TYPES = {
-    user: Field::BelongsTo,
-    category: Field::String,
+    article: Field::BelongsTo,
     id: Field::Number,
-    title: Field::String,
-    text: Field::Text,
-    created_at: Field::DateTime,
-    updated_at: Field::DateTime,
-    image: ImageField,
+    category: Field::String,
   }.freeze
 
   # COLLECTION_ATTRIBUTES
@@ -24,41 +19,31 @@ class ArticleDashboard < Administrate::BaseDashboard
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = [
-    :user,
-    :category,
+    :article,
     :id,
-    :title,
-    :text,
+    :category,
   ].freeze
 
   # SHOW_PAGE_ATTRIBUTES
   # an array of attributes that will be displayed on the model's show page.
   SHOW_PAGE_ATTRIBUTES = [
-    :user,
-    :category,
+    :article,
     :id,
-    :title,
-    :text,
-    :created_at,
-    :updated_at,
-    :image,
+    :category,
   ].freeze
 
   # FORM_ATTRIBUTES
   # an array of attributes that will be displayed
   # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = [
-    :user,
+    :article,
     :category,
-    :title,
-    :text,
-    :image,
   ].freeze
 
-  # Overwrite this method to customize how articles are displayed
+  # Overwrite this method to customize how categories are displayed
   # across all pages of the admin dashboard.
   #
-  # def display_resource(article)
-  #   "Article ##{article.id}"
+  # def display_resource(category)
+  #   "Category ##{category.id}"
   # end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -3,4 +3,5 @@ class Article < ApplicationRecord
   mount_uploader :image, ImageUploader
   belongs_to :user
   has_many :comments
+  has_many :categories
 end
