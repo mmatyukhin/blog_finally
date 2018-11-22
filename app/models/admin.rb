cfc91fa1d9bfd917582e26d0d68805d203980341
Mikhail Matyukhin <m.facepalm@gmail.com>
2018-11-22 22:50:47 +0300

    testing

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,5 +1,5 @@
 class Admin < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :rememberable, :validatable
+  devise :database_authenticatable, :rememberable, :validatable, :registerable
 end
