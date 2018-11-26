66ff4c1c51ddf045a3dacf8b7721035e539b0fb5
Mikhail Matyukhin <m.facepalm@gmail.com>
2018-11-26 20:51:07 +0300

    aws

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,16 +1,20 @@
+require 'carrierwave/storage/fog'
+
 CarrierWave.configure do |config|
-  if Rails.env.staging? || Rails.env.production?
-    config.fog_provider = ‘fog/aws’
-    config.fog_credentials = {
-      :provider => ‘AWS’,
-      :aws_access_key_id => ENV[‘AWS_ACCESS_KEY_ID’],
-      :aws_secret_access_key => ENV[‘AWS_SECRET_ACCESS_KEY’],
-      :region => ‘EU’ 
-    }
-    config.fog_directory = ENV[‘S3_BUCKET_NAME’]
+    if Rails.env.development? || Rails.env.test?
+      config.storage :file
+    else
+      config.storage = :fog
+      config.fog_credentials = {
+        provider: 'AWS',
+        aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+        aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+        region: 'EU'
+      }
+    config.fog_directory = 'adomantherokubucket'
     config.storage = :fog
-  else
+
     config.storage = :file
     config.enable_processing = Rails.env.development?
-  end
+    end
 end
