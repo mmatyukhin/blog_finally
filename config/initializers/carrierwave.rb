8a8cfa2bd58410c56e88cf4d2884632fcbf0d76c
Mikhail Matyukhin <m.facepalm@gmail.com>
2018-11-26 21:36:20 +0300

    aws

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,6 +8,6 @@ CarrierWave.configure do |config|
   config.aws_credentials = {
     access_key_id:     ENV.fetch('AWS_ACCESS_KEY_ID'),
     secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-    region:            ENV.fetch('AWS_REGION') # Required
+    region:            ENV.fetch('AWS_REGION')
   }
 end
