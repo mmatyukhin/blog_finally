de5f57b2c70cc423310154a8ca8af7a757bbe540
Mikhail Matyukhin <m.facepalm@gmail.com>
2018-11-26 14:01:30 +0300

    show page remade

--- app/controllers/admins/application_controller.rb
+++ app/controllers/admins/application_controller.rb
@@ -8,9 +8,9 @@
 class Admins::ApplicationController < Administrate::ApplicationController
   before_action :authenticate_admin!
 
-    # Override this value to specify the number of elements to display at a time
-    # on index pages. Defaults to 20.
-    # def records_per_page
-    #   params[:per_page] || 20
-    # end
-  end
+  # Override this value to specify the number of elements to display at a time
+  # on index pages. Defaults to 20.
+  # def records_per_page
+  #   params[:per_page] || 20
+  # end
+end
