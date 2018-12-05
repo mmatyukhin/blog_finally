129c8f9be9d8afd5fd54feb6d9413154bafef890
Mikhail Matyukhin <m.facepalm@gmail.com>
2018-12-05 23:13:03 +0300

    added new article button for users

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,4 +15,5 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
+//= require quill
 //= require_tree .
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,6 +7,17 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def create
+    @article = current_user.articles.build(article_params)
+
+    if @article.save
+      flash[:success] = "Article created!"
+      redirect_to root_url
+    else
+      render 'new'
+    end
+  end
+
   def like
     @article = Article.find(params[:id])
     @article.liked_by current_user
@@ -24,4 +35,9 @@ class ArticlesController < ApplicationController
       format.js { render layout: false }
     end
   end
+  private
+
+    def article_params
+      params.require(:article).permit(:title, :text, :image, :category_id)
+    end
 end
