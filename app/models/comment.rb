c0c475cb521c702e5711e9f86bc2a9999fda699e
Mikhail Matyukhin <m.facepalm@gmail.com>
2018-12-07 13:06:22 +0300

    comment length fixed

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,6 @@ class Comment < ApplicationRecord
   belongs_to :article
   belongs_to :user
   scope :approved, -> { where(moderation: true) }
-  validates :body, presence: true, length: { minimum: 50, maximum: 250 }
+  validates :body, presence: true
   acts_as_tree order: 'created_at DESC'
 end
