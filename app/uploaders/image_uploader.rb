7ac58da63d36065c4647d9bf6157f83eb53b0dfd
Mikhail Matyukhin <m.facepalm@gmail.com>
2019-02-16 12:20:20 +0300

    fixes for images

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,8 +1,12 @@
 class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
-  process resize_and_pad: [600, 300]
+  process resize_to_fit: [512, 512]
 
+  version :medium do
+    process resize_to_fill: [500, 300]
+  end
+  
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
