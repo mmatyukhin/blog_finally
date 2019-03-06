badc29f68534cdf5f092f44365842d330287e372
Mikhail Matyukhin <m.facepalm@gmail.com>
2019-03-06 14:20:53 +0300

    fix

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,5 +3,4 @@ class Comment < ApplicationRecord
   belongs_to :user
   scope :approved, -> { where(moderation: true) }
   validates :body, presence: true
-  acts_as_tree order: 'created_at DESC'
 end
